fc56d02eafffba1582a544be24bf2f4dec786043
dfish19 <dfish19@yahoo.com>
2022-08-30 16:05:36 -0500

    initial

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,21 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("bob", true)]
+        [InlineData("hannah", true)]
+        [InlineData("madam", true)]
+        [InlineData("Donte", false)]
+        public void Test1(string word, bool expected)
         {
-
+            var create = new WordSmith();
+            var actual = create.IsAPalindrome(word);
+            Assert.Equal(expected,actual);
         }
     }
 }
