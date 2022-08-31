a1d61526308ec02c0964486e0b5c3d1253bbde13
dfish19 <dfish19@yahoo.com>
2022-08-31 10:42:24 -0500

    initial

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+
+        public bool IsAPalindrome(string word)
+        {
+            return word.SequenceEqual(word.Reverse());
+        }
+    }
+}
